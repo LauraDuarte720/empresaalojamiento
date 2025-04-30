6daae376bd0e439f11c880f2e7656872e7c8081c
LauraDuarte720 <laurac.duarteq@uqvirtual.edu.co>
2025-04-29 22:34:25 -0500

    se crea el proyecto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module co.edu.uniquindio.empresaalojamiento {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.empresaalojamiento to javafx.fxml;
+    exports co.edu.uniquindio.empresaalojamiento;
+}
